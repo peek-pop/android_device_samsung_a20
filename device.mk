a354d8da96a03a68802408bf399ae439411e03d0
peek-pop <68726024+peek-pop@users.noreply.github.com>
2026-02-28 00:08:06 +0700

    Add bootctrl and Health Hal packages to device.mk

--- device.mk
+++ device.mk
@@ -8,3 +8,19 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PRODUCT_PROPERTIES += \
 	ro.fastbootd.available=true
+
+# bootctrl
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.2-impl \
+    android.hardware.boot@1.2-impl.recovery
+
+# Health Hal
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-impl.recovery \
+    android.hardware.health@2.1-service
+
+# Hide Reflash TWRP & FUSE passthrough
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.twrp.vendor_boot=true \
+    persist.sys.fuse.passthrough.enable
